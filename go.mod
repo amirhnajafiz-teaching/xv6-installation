d20013d66f765740397e50943cdc9fda4c3aa64c
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-13 08:38:40 +0430

    feat: tracer init

--- go.mod
+++ go.mod
@@ -6,13 +6,19 @@ require (
 	github.com/amirhnajafiz/stallion v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/knadh/koanf v1.4.3 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	go.opentelemetry.io/otel v1.10.0 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.10.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.10.0 // indirect
+	go.opentelemetry.io/otel/trace v1.10.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
-	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/sys v0.0.0-20220909162455-aba9fc2a8ff2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/telemetry/trace/trace.go
+++ internal/telemetry/trace/trace.go
@@ -1,1 +1,51 @@
 package trace
+
+import (
+	"log"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdk "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/semconv/v1.7.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func NewTracer() trace.Tracer {
+	exporter, err := jaeger.New(
+		jaeger.WithAgentEndpoint(jaeger.WithAgentHost("localhost"), jaeger.WithAgentPort(":3320")),
+	)
+	if err != nil {
+		log.Fatalf("failed to initialize export pipeline: %v", err)
+	}
+
+	res, err := resource.Merge(
+		resource.Default(),
+		resource.NewSchemaless(
+			semconv.ServiceNamespaceKey.String("snapp"),
+			semconv.ServiceNameKey.String("mqtt-blackbox-exporter"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	bsp := sdk.NewBatchSpanProcessor(exporter)
+	tp := sdk.NewTracerProvider(
+		sdk.WithSampler(sdk.ParentBased(sdk.TraceIDRatioBased(1.3))),
+		sdk.WithSpanProcessor(bsp),
+		sdk.WithResource(res),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	// register the TraceContext propagator globally.
+	var tc propagation.TraceContext
+
+	otel.SetTextMapPropagator(tc)
+
+	tracer := otel.Tracer("snapp/mqtt-blackbox-exporter")
+
+	return tracer
+}

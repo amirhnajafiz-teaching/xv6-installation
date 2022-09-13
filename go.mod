ec5159bf7f7902621e7e630db6172ed48f994921
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-13 08:29:39 +0430

    feat: logger

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/HighwayToVictory/stallion-load-test
 
 go 1.19
 
-require github.com/amirhnajafiz/stallion v1.1.1 // indirect
+require (
+	github.com/amirhnajafiz/stallion v1.1.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.23.0 // indirect
+)
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,17 @@
 package logger
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+)
+
+// NewLogger initializes a new logger interface.
+func NewLogger() (*zap.Logger, error) {
+	logger, err := zap.NewProduction(zap.Development())
+	if err != nil {
+		return nil, fmt.Errorf("zap initialize failed: %w", err)
+	}
+
+	return logger, nil
+}

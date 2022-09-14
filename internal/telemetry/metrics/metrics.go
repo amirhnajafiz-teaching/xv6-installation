13f76aeaf4787bc22cb2d05531e06838b31effd5
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-14 11:59:42 +0430

    feat: new metrics

--- internal/telemetry/metrics/metrics.go
+++ internal/telemetry/metrics/metrics.go
@@ -1,1 +1,16 @@
 package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func RunMetricsServer() {
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		if err := http.ListenAndServe(":2112", nil); err != nil {
+			panic(err)
+		}
+	}()
+}

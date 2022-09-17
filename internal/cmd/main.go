98fdc29ef4c8c7afdd2e72394679955b3276c3a4
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-17 09:49:01 +0430

    feat: main command

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"github.com/official-stallion/stallion-load-test/internal/cmd/consumer"
+	"github.com/official-stallion/stallion-load-test/internal/cmd/http"
+	"github.com/official-stallion/stallion-load-test/internal/cmd/publisher"
+	"github.com/official-stallion/stallion-load-test/internal/cmd/server"
+	"github.com/spf13/cobra"
+)
+
+func Execute() {
+	rootCmd := &cobra.Command{}
+
+	rootCmd.AddCommand(
+		consumer.GetCommand(),
+		http.GetCommand(),
+		publisher.GetCommand(),
+		server.GetCommand(),
+	)
+
+	if err := rootCmd.Execute(); err != nil {
+		panic(err)
+	}
+}

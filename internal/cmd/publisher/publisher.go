6d3c75911b2d97199004c04be9bee9dcaf90ffb9
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-17 09:47:12 +0430

    add: publisher command

--- internal/cmd/publisher/publisher.go
+++ internal/cmd/publisher/publisher.go
@@ -1,1 +1,17 @@
 package publisher
+
+import "github.com/spf13/cobra"
+
+func GetCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "publisher",
+		Short: "running publisher command",
+		Run: func(_ *cobra.Command, _ []string) {
+			main()
+		},
+	}
+}
+
+func main() {
+
+}

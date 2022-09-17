9f5df815a8c33bdf875d75d4aef4376f55e979d9
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-17 09:46:37 +0430

    add: http command

--- internal/cmd/http/http.go
+++ internal/cmd/http/http.go
@@ -1,1 +1,17 @@
 package http
+
+import "github.com/spf13/cobra"
+
+func GetCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "http",
+		Short: "starting http command",
+		Run: func(_ *cobra.Command, _ []string) {
+			main()
+		},
+	}
+}
+
+func main() {
+
+}

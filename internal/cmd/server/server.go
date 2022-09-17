7ec665d8756f3fba78ad7f796be0c673668d0a8d
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-17 09:47:39 +0430

    add: sever command

--- internal/cmd/server/server.go
+++ internal/cmd/server/server.go
@@ -1,1 +1,17 @@
 package server
+
+import "github.com/spf13/cobra"
+
+func GetCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "server",
+		Short: "starting server",
+		Run: func(_ *cobra.Command, _ []string) {
+			main()
+		},
+	}
+}
+
+func main() {
+
+}

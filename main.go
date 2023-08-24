dd197c2632fe711f75e2ebc84ea0ac6aedb37eb5
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-24 08:49:12 +0330

    update: main

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "github.com/official-stallion/race/internal/cmd"
+import (
+	"sync"
+
+	"github.com/official-stallion/race/internal/client"
+	"github.com/official-stallion/race/internal/config"
+)
 
 func main() {
-	cmd.Execute()
+	// load configs
+	cfg := config.Load()
+
+	// create consumers
+	j := 0
+	for i := 0; i < cfg.Consumers; i++ {
+		topic := cfg.Topics[j]
+
+		go client.Consumer(cfg.Host, topic)
+
+		j++
+		if j == len(cfg.Topics) {
+			j = 0
+		}
+	}
+
+	// create providers
+	j = 0
+	for i := 0; i < cfg.Providers; i++ {
+		topic := cfg.Topics[j]
+
+		go client.Provider(cfg.Host, topic, "hello world", 2)
+
+		j++
+		if j == len(cfg.Topics) {
+			j = 0
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	wg.Wait()
 }

fdf6a9aec07eefb5803d0286768ef41d00d9a8d2
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-24 08:43:57 +0330

    add: consumer

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,26 @@
 package client
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	sdk "github.com/official-stallion/go-sdk"
+)
+
+func Consumer(host, topic string) {
+	var wg sync.WaitGroup
+
+	client, err := sdk.NewClient(host)
+	if err != nil {
+		log.Println(fmt.Errorf("failed to connect error=%w", err))
+	}
+
+	wg.Add(1)
+
+	client.Subscribe(topic, func(bytes []byte) {
+		log.Println(fmt.Sprintf("got %d bytes", len(bytes)))
+	})
+
+	wg.Wait()
+}

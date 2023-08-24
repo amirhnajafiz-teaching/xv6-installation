b81e527969d488c78e156ddfb243a9f07dc7392c
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-24 09:04:54 +0330

    add: cli

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 
 	"github.com/official-stallion/race/internal/client"
 	"github.com/official-stallion/race/internal/config"
+	"github.com/official-stallion/race/internal/metrics"
 )
 
 func main() {
 	// load configs
 	cfg := config.Load()
 
+	// create new client
+	cli := client.Client{
+		Metrics: metrics.New(cfg.Providers, cfg.Consumers, cfg.Topics),
+	}
+
 	// create consumers
 	j := 0
 	for i := 0; i < cfg.Consumers; i++ {
 		topic := cfg.Topics[j]
 
-		go client.Consumer(cfg.Host, topic)
+		go cli.Consumer(cfg.Host, topic)
 
 		j++
 		if j == len(cfg.Topics) {
@@ -29,7 +35,7 @@ func main() {
 	for i := 0; i < cfg.Providers; i++ {
 		topic := cfg.Topics[j]
 
-		go client.Provider(cfg.Host, topic, cfg.Message, cfg.Offset)
+		go cli.Provider(cfg.Host, topic, cfg.Message, cfg.Offset)
 
 		j++
 		if j == len(cfg.Topics) {
